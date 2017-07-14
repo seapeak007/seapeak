58bb6e38726162066c40bec44a7cdcede8a2d60a
jianmin qi <qijianmin@lexue.com>
2017-07-14 19:26:12 +0800

    init

---
+++ stockdata/src/main/java/com/stockdata/BootApplication.java
@@ -0,0 +1,17 @@
+package com.stockdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ComponentScan("com.stockdata")
+@EnableScheduling
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+
+	}
+}
